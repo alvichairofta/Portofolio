63905ca17cc4ec5154bcd10f2c8913101d6200c8
alvichairofta <alviganteng14@gmail.com>
2024-01-07 17:53:17 +0700

    update portfolio

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -2,8 +2,10 @@ import { useSpring } from "@react-spring/core";
 import { animated } from "@react-spring/web";
 import React, { useState } from "react";
 import Mock from "../Assets/mockuuups.png";
+import Mock2 from "../Assets/Smpn4.png";
+import Mock3 from "../Assets/rest.png";
+import Mock4 from "../Assets/tdl.png";
 import "./portfolio.css";
-import Arrow from "../Assets/arrow.png";
 
 export default function Portfolio() {
   return (
@@ -14,7 +16,7 @@ export default function Portfolio() {
       </div>
       <div className="card">
         <div className="Detail" data-aos="fade-up" data-aos-duration="500">
-          <h2>Matara Website</h2>
+          <h2>CINEMATARA</h2>
           <h1>Movie Data Search Website, Especially Indonesian Movies</h1>
           <p>
             Complete and updated Indonesian movie data collection website. It is
@@ -34,7 +36,79 @@ export default function Portfolio() {
         <div className="Mockup" data-aos="fade-up" data-aos-duration="500">
           <img src={Mock} alt="Mockup-Matara" />
           <div className="visit-web">
-            <a href="https://cinemanusantara.xyz/">Visi website</a>
+            <a href="https://cinemanusantara.xyz/">Visit website</a>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="Detail" data-aos="fade-up" data-aos-duration="500">
+          <h2>SMPN 4 KOTA TANGERANG</h2>
+          <h1>School Website</h1>
+          <p>
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, possimus magnam, modi ducimus a odit in sit inventore quibusdam facere hic ipsum, mollitia fuga. Vel inventore at amet sit sapiente!
+          </p>
+          <button>
+            <a
+              href="https://smpn4kotatangerang.sch.id/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Visit Website
+            </a>
+          </button>
+        </div>
+        <div className="Mockup" data-aos="fade-up" data-aos-duration="500">
+          <img src={Mock2} alt="Mockup-Matara" />
+          <div className="visit-web">
+            <a href="https://smpn4kotatangerang.sch.id/">Visit website</a>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="Detail" data-aos="fade-up" data-aos-duration="500">
+          <h2>DeLouvre</h2>
+          <h1>Restaurant Website</h1>
+          <p>
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, possimus magnam, modi ducimus a odit in sit inventore quibusdam facere hic ipsum, mollitia fuga. Vel inventore at amet sit sapiente!
+          </p>
+          <button>
+            <a
+              href="https://if330-delouvre.xyz//"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Visit Website
+            </a>
+          </button>
+        </div>
+        <div className="Mockup" data-aos="fade-up" data-aos-duration="500">
+          <img src={Mock3} alt="Mockup-Matara" />
+          <div className="visit-web">
+            <a href="https://if330-delouvre.xyz/">Visit website</a>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="Detail" data-aos="fade-up" data-aos-duration="500">
+          <h2>TO DO LIST</h2>
+          <h1>TodoList Website</h1>
+          <p>
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, possimus magnam, modi ducimus a odit in sit inventore quibusdam facere hic ipsum, mollitia fuga. Vel inventore at amet sit sapiente!
+          </p>
+          <button>
+            <a
+              href="https://if330-todolist.xyz/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Visit Website
+            </a>
+          </button>
+        </div>
+        <div className="Mockup" data-aos="fade-up" data-aos-duration="500">
+          <img src={Mock4} alt="Mockup-Matara" />
+          <div className="visit-web">
+            <a href="https://if330-todolist.xyz/">Visit website</a>
           </div>
         </div>
       </div>
